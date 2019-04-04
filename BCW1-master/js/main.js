6711d9b668e5b505710954f72357ae73c7af3f73
wilhelmo360 <32298091+wilhelmo360@users.noreply.github.com>
2019-04-04 23:35:14 +0300

    Add files via upload

---
+++ BCW1-master/js/main.js
@@ -0,0 +1,20 @@
+
+function(f){
+
+var form = document.getElementById("formula");
+
+	if (form.addEventListener){
+		form.addEventListener("submit", function(callback), false);
+	}
+
+		}
+function(callback){
+		document.querySelector("formula").addEventListener("submit", function(f){
+		
+			if(!true){
+				e.preventDefault();
+			}
+	}
+	
+
+}
